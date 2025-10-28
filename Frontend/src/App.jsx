2adfb3f2d75ca0d620f863620a365771a8e3f4df
Jwala Gupta <jwalagupta58@gmail.com>
2025-10-28 18:56:42 +0530

    Refactor frontend structure: update title, add routing, implement Navbar, and create essential pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contect";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Order from "./Pages/Order";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Product from "./Pages/Product";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="main_div">
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/placeOrder" element={<PlaceOrder />} />
+          <Route path="/product/:ProductId" element={<Product />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { NavLink, Link } from "react-router-dom";
+import { IoMdSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { IoBagHandleSharp } from "react-icons/io5";
+import { TiThListOutline } from "react-icons/ti";
+import { IoIosArrowBack } from "react-icons/io";
+import { assets } from "../../assets/frontend_assets/assets.js";
+
+const Navbar = () => {
+  const [navVisible, setNavVisible] = useState(false);
+
+  return (
+    <nav>
+      <div className="navbar border-bottom border-1 d-flex justify-content-between align-items-center ">
+        <Link to={"/"}>
+          <img src={assets.logo} className="brand_logo" alt="" />
+        </Link>
+        <div className="nav_ul_div">
+          <ul className="nav_ul d-flex list-unstyled">
+            <NavLink to={"/"}>
+              <p className="nav_p">HOME</p>
+            </NavLink>
+            <NavLink to={"/collection"}>
+              <p className="nav_p">COLLECTION</p>
+            </NavLink>
+            <NavLink to={"/about"}>
+              <p className="nav_p">ABOUT</p>
+            </NavLink>
+            <NavLink to={"/contact"}>
+              <p className="nav_p">CONTACT</p>
+            </NavLink>
+          </ul>
+        </div>
+        <div className="nav_right_div d-flex">
+          <span>
+            <IoMdSearch />
+          </span>
+          <span className="user_icon position-relative">
+            <FaRegUser />
+            <div className="user_hover_div d-none">
+              <div className="hover_box border rounded d-flex flex-column fs-5 position-absolute">
+                <div className="hovor_1">My Profile</div>
+                <div className="hovor_2">My Orders</div>
+                <div className="hovor_3">Logout</div>
+              </div>
+            </div>
+          </span>
+          <span className="position-relative">
+            <Link to={"/cart"}>
+              <IoBagHandleSharp />
+              <p className="position-absolute top-100 start-100 translate-middle badge rounded-pill">
+                0
+              </p>
+            </Link>
+          </span>
+          <span className="three_dot_menu" onClick={() => setNavVisible(true)}>
+            <TiThListOutline />
+          </span>
+        </div>
+      </div>
+      <div
+        className={`drop_menu bg-light position-absolute top-0 start-0 bottom-0 overflow-hidden w-100 h-100 ${
+          navVisible ? "d-block" : "d-none"
+        }`}
+      >
+        <div className="d-flex flex-column">
+          <div
+            onClick={() => setNavVisible(false)}
+            className="nav_back_button my-3 mx-3"
+          >
+            <span>
+              <IoIosArrowBack /> Back
+            </span>
+          </div>
+          <NavLink
+            className="py-2 ps-4 border"
+            onClick={() => setNavVisible(false)}
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="py-2 ps-4 border"
+            onClick={() => setNavVisible(false)}
+            to="/collection"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            className="py-2 ps-4 border"
+            onClick={() => setNavVisible(false)}
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            className="py-2 ps-4 border"
+            onClick={() => setNavVisible(false)}
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
