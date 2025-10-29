f60a0d974e1d392584bf00fc56a7865f1d08d4f9
Jwala Gupta <jwalagupta58@gmail.com>
2025-10-29 17:24:20 +0530

    Add Footer component import to About page; prepare for future integration

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import AboutHero from "../Components/AboutHero/AboutHero";
 import AboutMainDescription from "../Components/About_description/AboutMainDescription";
 import AboutMainWhy from "../Components/AboutWhy/AboutMainWhy";
+import NewslatterBox from "../Components/NewsLatterBox/NewslatterBox";
+import Footer from "../Components/Footer/Footer";
 
 const About = () => {
   return <div className="about_page">
     <AboutHero />
     <AboutMainDescription />
     <AboutMainWhy />
+    <NewslatterBox />
+    {/* <Footer /> */}
   </div>;
 };
 
