7c2f0e371bdc8d3727429cd9f01be13570793c1c
Jwala Gupta <jwalagupta58@gmail.com>
2025-11-03 23:27:32 +0530

    Add initial backend setup with server configuration, environment variables, and .gitignore

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// App config
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoint
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`your server is running on http://localhost:${port}`);
+});
