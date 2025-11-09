d812f9d50e028498a9255faee2b141ecc6d33661
Jwala Gupta <jwalagupta58@gmail.com>
2025-11-09 12:42:57 +0530

    orderModel schema completed

---
+++ Backend/Model/OrderModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      require: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "Order Placed",
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    payment: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    date: {
+      type: Number,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const OrderModel =
+  mongoose.models.Schema || mongoose.model("OrderModel", orderSchema);
